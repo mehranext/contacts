a3c67d684e8ffc61c1c6f65fabfd30e77578bdd4
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-07-02 11:04:59 +0430

    Explain validate for enter in our textField and wrap our scaffold to form and create key for them and explain some conditions for our floating action button

--- lib/screens/add_edit_screen.dart
+++ lib/screens/add_edit_screen.dart
@@ -1,6 +1,11 @@
+import 'package:contacts/widgets/MyButton.dart';
+import 'package:contacts/widgets/MyTextField.dart';
 import 'package:flutter/material.dart';
 
 class AppEditScreen extends StatefulWidget {
+  static TextEditingController nameController = TextEditingController();
+  static TextEditingController phoneController = TextEditingController();
+
   const AppEditScreen({super.key});
 
   @override
@@ -8,17 +13,52 @@ class AppEditScreen extends StatefulWidget {
 }
 
 class _AppEditScreenState extends State<AppEditScreen> {
+  final FormKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.lightGreen,
-        title: const Text(
-          'مخاطب جدید',
-          style: TextStyle(fontSize: 18),
+    return Form(
+      key: FormKey,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightGreen,
+          title: const Text(
+            'مخاطب جدید',
+            style: TextStyle(fontSize: 18),
+          ),
+          centerTitle: true,
+          elevation: 0,
         ),
-        centerTitle: true,
-        elevation: 0,
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              MyTextField(
+                controller: AppEditScreen.nameController,
+                errorText: 'Enter Your Name',
+                hintText: 'Name',
+                inputType: TextInputType.name,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              MyTextField(
+                controller: AppEditScreen.phoneController,
+                errorText: 'Enter Your Phone Number',
+                hintText: 'phone',
+                inputType: TextInputType.number,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              MyButton(
+                  child: Text('Add'),
+                  width: 350,
+                  onPressed: () {
+                    if (FormKey.currentState!.validate()) {}
+                  })
+            ],
+          ),
+        ),
       ),
     );
   }
